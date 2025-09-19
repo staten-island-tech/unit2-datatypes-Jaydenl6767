6167c4f73161713d406e4dec9b830acaf74af390
Jaydenl6767 <Jaydenl321@nycstudents.net>
2025-09-19 06:24:24 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,67 @@
+# x = 3
+# y = float(3)
+# print(x,y)
+# values = [1,2.23,5,7,2,30,15]
+# print(values)
+# for i in values:
+#     print(i)
+# print(values[0])
+# print(values[6])
+
+# x = "this is a thing"
+# y= x.split( )
+# z = y[0]
+# print(y)
+# print(z)
+
+# '''if day_of_week == "Friday" or "friday":
+#     print("incorrect")
+# else:
+#     print("correct")
+
+# x = "test"
+# print(f"hello {x}")
+
+# temp = 67
+# if temp > 68:
+#     print('warm')
+# elif temp == 68:
+#     print('perfect')
+# else:
+
+# print('cold')
+
+
+# def odd_even(x):
+#     if x % 2 ==0:
+#         print('This number is even')
+#     else: 
+#         print('This number is odd')
+# odd_even()'''
+
+def y(y):
+    print (y * 2)
+y(5)
+
+def ser(s):
+    def bill(b):
+    if s == ("bad"):
+        def tip(x):
+            print(b * x)
+            bill(10)
+            tip(0)
+    
+    elif s == ("okay"):
+       def tip (x):
+        print(b * x)
+        tip(15)
+    elif s == ("good"):
+    def tip (x):
+        print(b * x)
+        tip(20)
+    elif s == ("Great"):
+        def tip (x):
+        print(b * x)
+        tip(25)
+    ser("bad")
+    
